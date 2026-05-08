1dacfeab8edcc8b47b57854b463cef012e81d39a
Joan <joaandp117@gmail.com>
2026-05-08 12:02:18 +0200

    Añado conexion con postgre con HikariCP, tambien añado enter para seguir en listados , fixes menores

--- JavaPillam/src/Main.java
+++ JavaPillam/src/Main.java
@@ -1,28 +1,19 @@
 import Model.DAO.*;
+import Model.DAO.Clases.ActualitzarEstatsDAO;
+import Model.DAO.Clases.*;
 import Vista.*;
 import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-
-                try {
-                    // Fuerza la conexión al arrancar
-                    DBConnection.getConnection();
-
-                    // Lanzas el menú principal
-                    menuPrincipal menu = new menuPrincipal();
-                    menu.mostrar();
-
-                } catch (SQLException e) {
-                    System.out.println("Error conectant a la BDD: " + e.getMessage());
-                } finally {
-                    // Se ejecuta siempre, tanto si hay error como si no
-                    try {
-                        DBConnection.closeConnection();
-                    } catch (SQLException e) {
-                        System.out.println("Error tancant la connexió: " + e.getMessage());
-                    }
-                }
-            }
+        try {
+            DBConnection.getConnection();
+            ActualitzarEstatsDAO.comprovarIActualitzar(); // <- aquí
+            menuPrincipal menu = new menuPrincipal();
+            menu.mostrar();
+        } catch (Exception e) {
+            System.out.println("Error conectant a la BDD: " + e.getMessage());
+        } finally {
+            DBConnection.closePool();
         }
-
-
+    }
+}
